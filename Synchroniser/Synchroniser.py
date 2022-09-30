c7ff20c16045699658731aba2a0fc488d67f7dff
Gulzar22 <gulzarneelesh@gmail.com>
2022-09-30 22:25:29 +0200

    Add project files.

---
+++ Synchroniser/Synchroniser.py
@@ -0,0 +1,13 @@
+import hashlib
+
+
+class Digest:
+    filePath=""
+    
+    def readFileContent():
+        #use the file path to get the file
+        return fileData
+
+    
+    def getDigest(fileData):
+        return fileDigest
